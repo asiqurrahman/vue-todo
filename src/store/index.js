79d45818220cd6ccf2216e73aa702ecf5ad0a705
asiqurrahman <asiqurrahman13@gmail.com>
2022-07-14 12:20:23 -0400

    git push

--- src/store/index.js
+++ src/store/index.js
@@ -2,17 +2,19 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
-    count: 0
-  },
-  getters: {
-
+    count: 0,
   },
   mutations: {
-
+    // $store.commit('increase')
+    increase(state) {
+      state.count++;
+    },
   },
   actions: {
-    
+    // api calls
+    // commit('increase', response.data)
+    // $store.dispatch('increase')
   },
-  modules: {
-  }
-})
+  getters: {},
+  modules: {},
+});
